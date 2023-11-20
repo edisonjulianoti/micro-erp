2603da194f701340acd1830248fab00becfb7ae1
builder <builder@madbuilder.com.br>
2023-11-20 13:26:54 +0000

    Retirar o Gerar dados

--- app/database/inserts-permission.sql
+++ app/database/inserts-permission.sql
@@ -54,8 +54,6 @@ INSERT INTO system_program (id,name,controller) VALUES( (SELECT max(p.id) + 1 FR
 INSERT INTO system_group_program (id, system_group_id, system_program_id) VALUES( (SELECT max(gp.id) + 1 FROM system_group_program gp), (SELECT max(g.id) FROM system_group g) , (SELECT max(p.id) FROM system_program p where p.controller = 'ProdutoForm'));
 INSERT INTO system_program (id,name,controller) VALUES( (SELECT max(p.id) + 1 FROM system_program p) , 'Cadastro de problema', 'ProblemaForm');
 INSERT INTO system_group_program (id, system_group_id, system_program_id) VALUES( (SELECT max(gp.id) + 1 FROM system_group_program gp), (SELECT max(g.id) FROM system_group g) , (SELECT max(p.id) FROM system_program p where p.controller = 'ProblemaForm'));
-INSERT INTO system_program (id,name,controller) VALUES( (SELECT max(p.id) + 1 FROM system_program p) , 'Gerar Dados', 'GerarDadosController');
-INSERT INTO system_group_program (id, system_group_id, system_program_id) VALUES( (SELECT max(gp.id) + 1 FROM system_group_program gp), (SELECT max(g.id) FROM system_group g) , (SELECT max(p.id) FROM system_program p where p.controller = 'GerarDadosController'));
 INSERT INTO system_group (id, name, uuid) VALUES( (SELECT max(g.id) + 1 FROM system_group g) , 'Pessoas', 'd8690165-6028-492f-801a-dc9ba9e6d3b0');
 INSERT INTO system_user_group (id, system_group_id, system_user_id) VALUES((SELECT max(ug.id) + 1 FROM system_user_group ug), (SELECT max(g.id) FROM system_group g), 1);
 INSERT INTO system_program (id,name,controller) VALUES( (SELECT max(p.id) + 1 FROM system_program p) , 'Gerenciar', 'PessoaList');
